09da18860d841ad0bb5882aad29f41a0946f364a
Kaynan Teixeira <kaynan2817@gmail.com>
2024-12-04 22:52:24 -0300

    validação de email do formulário do footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,19 @@ const menuMobile = document.querySelector(".menu-mobile")
 const btnHamburguer = document.querySelector(".menu-h")
 const imgHamburguer = document.querySelector(".hamburguer")
 const body = document.querySelector('.body')
+const form = document.querySelector('#form-footer')
+const formInput = document.querySelector('#form-text')
+const btnForm = document.querySelector('#form-btn')
+const msg = document.querySelector('.mensagem')
+// funções
+
+function validEmail(email){
+    
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email)
+}
+
+// eventos
 btnHamburguer.addEventListener('click', () =>{
     
     if(menuMobile.classList.contains('active')){
@@ -16,4 +29,19 @@ btnHamburguer.addEventListener('click', () =>{
     }
 })
 
-console.log(menuMobile, btnHamburguer, imgHamburguer)
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const email = formInput.value
+
+    if(validEmail(email)){
+        msg.innerHTML = 'Enviado com sucesso!'
+        msg.style.color = 'green'
+    
+    } else{
+        msg.innerHTML = 'Ops! Email inválido!'
+        msg.style.color = 'red'
+        return false
+    }
+})
